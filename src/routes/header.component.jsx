056c7c59c2d1d4cf6380c0af3c37b7a59aad13a7
Oztobuzz <oanh.tranotsc1123@hcmut.edu.vn>
2022-11-30 23:41:20 +0700

    Update Navigation bar

--- src/routes/header.component.jsx
+++ src/routes/header.component.jsx
@@ -1,15 +1,15 @@
 
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () =>{
     return(
         <div className="container">
             <ul>
-                <li><a href="#"><i className="fa fa-tachometer"></i>DASHBOARD</a></li>
-                <li><a href="worker"><i className="fa fa-male"></i>WORKER</a></li>
-                <li><a href="vehicle"><i className="fa fa-truck"></i>VEHICLE</a></li>
-                <li><a href="mcp"><i className="fa fa-trash"></i>MCP</a></li>
+                <li><a href="calendar.html"><i className="fa fa-tachometer"></i>DASHBOARD</a></li>
+                <li><Link to = "/worker"><i className="fa fa-male"></i>WORKER</Link></li>
+                <li><Link to = "/vehicle"><i className="fa fa-truck"></i>VEHICLE</Link></li>
+                <li><Link to = "/mcp"><i className="fa fa-trash"></i>MCP</Link></li>
                 <li><a href="#"><i className="fa fa-phone"></i>CONTACT</a></li>
                 <li><a href="Login.html"><i className="fa fa-sign-out"></i>LOGOUT</a></li>
             </ul>
